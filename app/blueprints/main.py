eaf1dec225565938aeb71272e0823ef734c4beb5
Bender <bender@Benders-MBP.attlocal.net>
2020-08-28 07:59:34 -0500

    ProjectPage

--- app/blueprints/main.py
+++ app/blueprints/main.py
@@ -9,4 +9,15 @@ def index():
 @main.route('/contact')
 def contact():
     return render_template('contact.html')
+
+@main.route('/project')
+def project():
+    return render_template('projects.html')
+
+# @main.route('/test')
+# @main.route('/test/<poopy>')
+# def test(poopy='biatch'):
+#     somelist = [1,2,3,4,5]
+#     print (poopy)
+#     return render_template('test.html', somelist=somelist, peepee=poopy)
     
