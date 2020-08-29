fc1ab6a59df24710f6950c208fa007383d2c2034
max.bender1 <max.bender1@ibm.com>
2020-08-29 16:42:51 -0400

    initial database code with tags example

--- app/blueprints/main.py
+++ app/blueprints/main.py
@@ -1,20 +1,56 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from random import shuffle
+
+from flask import Blueprint, redirect, render_template, request, url_for
+
+from app import db
+from app.models import Project, Tag
 
 main = Blueprint('main', __name__, template_folder='../templates')
 
+
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
+
 @main.route('/')
 def index():
-    return render_template('index.html')
 
-@main.route('/contact')
+    # Query all Tags and Shuffle the output
+    all_tags = Tag.query.all()
+    shuffle(all_tags)
+
+    # Split tags into 3 different lists https://stackoverflow.com/questions/312443/how-do-you-split-a-list-into-evenly-sized-chunks
+    split_tag_list = list(chunks(all_tags, int(len(all_tags) / 3)))
+
+    print(split_tag_list)
+    print(type(split_tag_list))
+    first_tag_list = split_tag_list[0]
+    second_tag_list = split_tag_list[1]
+    third_tag_list = split_tag_list[2]
+
+    if len(split_tag_list) > 3:
+        third_tag_list.extend(split_tag_list[3])
+
+    return render_template('index.html',
+                           first_tag_list=first_tag_list,
+                           second_tag_list=second_tag_list,
+                           third_tag_list=third_tag_list
+                           )
+
+
+@ main.route('/contact')
 def contact():
     return render_template('contact.html')
 
-@main.route('/project')
+
+@ main.route('/project')
 def project():
     return render_template('project.html')
-    
-@main.route('/projectdetail')
+
+
+@ main.route('/projectdetail')
 def projectdetail():
     return render_template('projectdetail.html')
 
@@ -24,4 +60,3 @@ def projectdetail():
 #     somelist = [1,2,3,4,5]
 #     print (poopy)
 #     return render_template('test.html', somelist=somelist, peepee=poopy)
-    
--- app/blueprints/projects.py
+++ app/blueprints/projects.py
@@ -1,10 +1,27 @@
 from flask import Blueprint, render_template, request, redirect, url_for
+from app.models import Project, Tag
+from app import db
 
 projects = Blueprint('projects', __name__, template_folder='../templates')
+
 
 @projects.route('/')
 def hello():
     return 'hello'
 
+
+@projects.route('/create/<title>/<short_description>/<long_description>')
+def create(title, short_description, long_description):
+    project1 = Project(title, short_description, long_description)
+    db.session.add(project1)
+    db.session.commmit()
+    return 'create'
+
+
+@projects.route('/view/<title>')
+def view(title):
+    project1 = Project.query.filter_by(title=title).first()
+
+    return render_template("project-base.html", project=project1)
 
     # url_for(projects.hello) for referencing projects within blueprint
