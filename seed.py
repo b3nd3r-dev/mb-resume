7c3dfc41d591f3824fca950ba8918612f1dbde39
Bender <bender@Benders-MBP.attlocal.net>
2020-11-25 08:36:15 -0600

    resume

--- seed.py
+++ seed.py
@@ -28,64 +28,30 @@ tags = [
 ]
 
 projects = [
-    {'title': 'IBM Cloud Private',
-     'short_description': 'IBM Cloud™ Private is a reliable and scalable cloud platform that runs on your infrastructure. It’s built on open source frameworks, like containers, Kubernetes and Cloud Foundry.',
-     'long_description': 'IBM Cloud™ Private is a reliable and scalable cloud platform that runs on your infrastructure. It’s built on open source frameworks, like containers, Kubernetes and Cloud Foundry.LONG VERSION',
-     'project_link': 'https://www.ibm.com/cloud/private',
+    {'title': 'Delete Me',
+     'short_description': 'Delete Me',
+     'long_description': 'Delete Me',
+     'project_link': 'Delete Me',
      'featured': True,
      'tags': 'Kubernetes,Docker'
      },
-
-    {'title': 'Travis on Z',
-     'project_link': 'github.com/bendermIBM/travis-cookbooks',
-     'short_description': 'Enabling the tool that enables the tools - on Linux on Z.',
-     'long_description': 'Enabling the tool that enables the tools - on Linux on Z.LONG VERSION',
+    {'title': 'Delete Me2',
+     'short_description': 'Delete Me',
+     'long_description': 'Delete Me',
+     'project_link': 'Delete Me',
      'featured': True,
-     'tags': 'Travis'
-     },
-    {'title': 'NexNest',
-     'project_link': 'github.com/maxbbender/nexnest',
-     'short_description': 'Student housing portal built with love by @Max Bender and @Mike Mcginnis',
-     'long_description': 'I had written a couple websites with Flask in school, but wanted to learn more and read up on Miguel Grinberg’s Flask tutorial and was exposed to a new set of best practices. Approached with an idea for a rental website targeted at students we designed and implemented a website written in Python. Backed by a fairly complex database that featured notifications and recursive comments, SQLAlchemy handles most of the direct SQL queries the complex JOINs.',
-     'featured': True,
-     'tags': 'Kubernetes,Python'
-     },
-    {'title': 'Viral Education/TALOS',
-     'project_link': 'github.com/maxbbender/Viral-Education',
-     'short_description': 'TALOS (Textual Augmentation Learning of Semantics), a Web-based platform for vocabulary acquisition that provides capabilities for analytics to support learning of vocabulary. This software prototype, currently in beta-testing, supports differentiated learning as well as common coursework, allowing users to contribute and access text while promoting ease of reading.',
-     'long_description': 'The first real website I made for someone other than myself. Written in PHP in a summer alongside working at the Marist College HelpDesk, this website was part of a research project that funded me to present at INTED in Madrid, Spain. Alongside two professors, I gave a presentation on using technology to enhance learning in an un-obtrusive fashion with a concentration in learning foreign languages.'
-
-     'Backed by a MySQL database this is where I learned a lot about security in the modern age of web-development. Certain vulnerabilities such as SQL Injections and XSS were ones that I focused on researching because I knew they were some of the more common avenues of attack. Looking back on it however I have already noticed some mistakes but I was happy with how it came out.',
-     'featured': True,
-     'tags': 'PHP'
-     },
-    {'title': 'Let\'s Talk',
-     'project_link': '',
-     'short_description': 'Short hands-on education modules surrounding a specific technology or a group of technologies together.',
-     'long_description': 'Teaching others and seeing that lightbulb go off is and experience that I get a lot of gratification from. In my effort to educate teams at IBM I realized that giving the same presentation to team after team was an inefficient method. So I created “Let’s Talk” which is a group of education modules that have an intense focus on hands-on learning using examples hosted on GitHub. We scheduled live presentations and recorded them for future consumption and they featured code examples accompanied by a tutorial-like presentation.'
-
-     'It began with connecting the travis-ci worker to an Openstack instance hosted on z/KVM. However we ran into issues with big bursts of builds which caused the networking layer in Openstack to intermittently not initiate. Eventually we upgraded to use LXD to run the builds which gave a huge speed increase and more portability. Using Packer I built xenial/bionic base images for Linux on Z and deployed a production configuration supporting 20,000+ builds a year.'
-
-     'See some changes to the travis-build scripts which also needed some configuartions.'
-
-     'Once I moved to the LXD based builds I was able to automate the deployment of LXD and the Travis Worker on a Ubuntu Bionic host using Ansible. Still using the previous OpenStack environment we could now provision infrastructure with Terraform and configure workers using Ansible.',
-     'featured': False,
-     'tags': ''
+     'tags': 'Kubernetes,Docker'
      }
 ]
 
 collabs = [
-    {'fname': 'Max', 'lname': 'Bender', 'name': 'MaxBender', 'clink': 'linkMB'},
-    {'fname': 'Zac', 'lname': 'Bender', 'name': 'ZacBender', 'clink': 'linkZB'},
-    {'fname': 'Kasey', 'lname': 'Leahy', 'name': 'KaseyLeahy', 'clink': 'linkKL'},
-    {'fname': 'Tom', 'lname': 'Rowles', 'name': 'TomRowles', 'clink': 'linkTR'},
 ]
 
 aboutmes = [
     {'title': 'Principal Engineer',
      'subtitle': "I'm Max Bender a Principal Engineer specializing in CICD and DevOps solutions.",
-     'description': 'Primarily, my work has been on CICD and DevOps solutions, but I often get this itch to create something new. Python is my go-to scripting language but I’m not afraid of a little Bash or Ruby if the use-case calls for it or I am trying to work with a library that requires the aforementioned (i.e Chef). The instant gratification of making websites and working with complex relational database setups is a puzzle that I relish. Using Flask and SQLAlchemy I have put together some complex websites as a consultant on the side. See the NexNest project as an example.',
-     'quote': '[being a Principal Engineer] is far more strategic, it’s far more business oriented, it’s involving a lot more influence, being an influencer without having the management title. [during the time becoming a Principal Engineer you] need to be a force multiplier. There was a clear transition from me writing code to teaching others how to write code that’s actually clean and doesn’t cause us trouble too fast down the line. One of the big parts of Principal Engineering in my mind is you teach more than you actually do yourself.'}
+     'description': '',
+     'quote': ''}
 ]
 
 achievements = [
@@ -127,7 +93,7 @@ achievements = [
 ]
 
 users = [
-    {'username': 'admin', 'password': 'asdf1234'}
+    {'username': 'bender', 'password': 'HXGsJuDBppq1gixF4LGn'}
 ]
 
 
